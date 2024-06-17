57ced0dc46a8a7199ee1f2b66286c2ccf0943090
Larissa Djunbawski <larydjm@gmail.com>
2024-06-17 08:44:15 -0300

    priemiro commit

---
+++ index.js
@@ -0,0 +1,14 @@
+console.log("  Digite um número para gerar sua tabuada ")
+let entrada_usuario = null;
+let tabuada = [];
+
+process.stdin.on("data", function(data) {
+    if (!entrada_usuario) { entrada_usuario = Number(data.toString().trim());
+
+        for (let i = 1; i <= 10; i++) {
+            let resultado = i * entrada_usuario;
+            tabuada.push(resultado); // Adiciona o resultado ao array tabuada
+            console.log(entrada_usuario +"x"+ i + "=" + resultado)
+        }
+    }
+});
